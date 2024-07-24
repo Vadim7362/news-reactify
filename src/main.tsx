ff37b002f6936c994624e55454f236658b6fe970
Vadim Cherepanov <Vadim5005473@mail.ru>
2024-07-24 20:32:47 +0300

    fix bag

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
-import { Provider } from "react-redux";
 import { store } from "./store/index";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
