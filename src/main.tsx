ce17a8035b9c2bc16a3cf6c2d990f2688206a7d5
Vadim Cherepanov <Vadim5005473@mail.ru>
2024-07-24 13:08:22 +0300

    bag

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
